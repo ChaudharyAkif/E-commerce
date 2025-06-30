565e5c04f651aaa0c3f40c3a76d4c05514df9796
Muhammad Akif Hussain <makifhussainmn@gmail.com>
2025-06-30 13:14:59 +0500

    five  Commit

--- src/config/supabase.jsx
+++ src/config/supabase.jsx
@@ -2,8 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 
 const VITE_SUPABASE_URL = "https://qhntcmrfiukxnhvwrovh.supabase.co";
 const VITE_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFobnRjbXJmaXVreG5odndyb3ZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk4MzI3MzMsImV4cCI6MjA2NTQwODczM30.iNYSAN_h3bChALkwWptUYY3FyZtCorsaWuVqE7FQUHI";
-const VITE_SITE_URL = "https://e-commerce-gwvo.vercel.app"; // Updated to your vercel domain
-
+const VITE_SITE_URL = "https://e-commerce-thag.vercel.app"; // Updated to your vercel domain
+                      
 const supabase = createClient(VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY, {
   auth: {
     autoRefreshToken: true,
